749ea7662e277663f2781eb097834013c7c1f7e3
“jmfonsecap” <jmfonsecap@eafit.edu.co>
2023-05-30 13:21:53 -0500

    Punto2.1

--- dane-punto1.py
+++ dane-punto1.py
@@ -2,14 +2,15 @@ from mrjob.job import MRJob
 
 class MinMaxStockPrice(MRJob):
     def mapper(self, _, line):
-        company, price, date = line.split(',')
-        yield company, (float(price), date)
+        for w in line.split():
+            filing = w.split(',')
+            yield filing[0],(float(filing[1]), filing[2])
     
     def reducer(self, key, values):
         prices = list(values)
-        min_price = min(prices[0])
-        max_price = max(prices[0])
-        yield key, (min_price[1], max_price[1])
+        min_price = min(prices)
+        max_price = max(prices)
+        yield key, (min_price[1],max_price[1])
 
 if __name__ == '__main__':
     MinMaxStockPrice.run()
