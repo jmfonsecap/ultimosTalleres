a9530a17e18bec8368614c49469484a15d3ad62b
“jmfonsecap” <jmfonsecap@eafit.edu.co>
2023-05-30 12:30:35 -0500

    punto2

--- dane-punto1.py
+++ dane-punto1.py
@@ -1,49 +1,15 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
 
-class SalaryStatistics(MRJob):
+class MinMaxStockPrice(MRJob):
+    def mapper(self, _, line):
+        company, price, date = line.split(',')
+        yield company, (float(price), date)
     
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper_get_salary,
-                   reducer=self.reducer_average_salary),
-            MRStep(mapper=self.mapper_get_employee,
-                   reducer=self.reducer_employee_statistics)
-        ]
-    
-    def mapper_get_salary(self, _, line):
-        
-        idemp, sececon, salary, year = line.strip().split(',')
-        yield sececon, float(salary)
-    
-    def reducer_average_salary(self, sececon, salaries):
-        total_salaries = 0
-        num_employees = 0
-        for salary in salaries:
-            total_salaries += salary
-            num_employees += 1
-        yield sececon, total_salaries / num_employees
-    
-    def mapper_get_employee(self, _, line):
-        idemp, sececon, salary, year = line.strip().split(',')
-
-        yield idemp, float(salary)
-    
-    def reducer_employee_statistics(self, idemp, salaries):
-        total_salaries = 0
-        num_jobs = 0
-        for salary in salaries:
-            total_salaries += salary
-            num_jobs += 1
-        yield idemp, total_salaries / num_jobs
-    
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper_get_salary,
-                   reducer=self.reducer_average_salary),
-            MRStep(mapper=self.mapper_get_employee,
-                   reducer=self.reducer_employee_statistics)
-        ]
+    def reducer(self, key, values):
+        prices = list(values)
+        min_price = min(prices[0])
+        max_price = max(prices[0])
+        yield key, (min_price[1], max_price[1])
 
 if __name__ == '__main__':
-    SalaryStatistics.run()
+    MinMaxStockPrice.run()
